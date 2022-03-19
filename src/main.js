6c63d75cb751e3bb7ee88a1fb4f59082b2b72ab5
qwq-789 <elfrain520@gmail.com>
2022-03-20 00:07:26 +0800

    v4 navbar done

--- src/main.js
+++ src/main.js
@@ -1,39 +1,15 @@
 import './style.css'
 
-var burgerButton = document.getElementById("bb");
-var burger = document.getElementById("burger");
-burgerButton.addEventListener("click", function () {
-  burger.classList.toggle("-translate-y-72");
-  burger.classList.toggle("translate-y-10");
-  burger.classList.toggle("fixed");
-});
-var list_a = document.getElementById("list-a");
-var list_b = document.getElementById("list-b");
-var list_c = document.getElementById("list-c");
-var list_d = document.getElementById("list-d");
-var list_e = document.getElementById("list-e");
-list_a.addEventListener("click", function () {
-  burger.classList.toggle("-translate-y-72");
-  burger.classList.toggle("translate-y-10");
-  burger.classList.toggle("fixed");
-});
-list_b.addEventListener("click", function () {
-  burger.classList.toggle("-translate-y-72");
-  burger.classList.toggle("translate-y-10");
-  burger.classList.toggle("fixed");
-});
-list_c.addEventListener("click", function () {
-  burger.classList.toggle("-translate-y-72");
-  burger.classList.toggle("translate-y-10");
-  burger.classList.toggle("fixed");
-});
-list_d.addEventListener("click", function () {
-  burger.classList.toggle("-translate-y-72");
-  burger.classList.toggle("translate-y-10");
-  burger.classList.toggle("fixed");
-});
-list_e.addEventListener("click", function () {
-  burger.classList.toggle("-translate-y-72");
-  burger.classList.toggle("translate-y-10");
-  burger.classList.toggle("fixed");
-});
+var bb = document.getElementById('bb');
+var burger = document.getElementById('burger');
+var header = document.getElementsByTagName('header')
+
+bb.onclick = function(){
+  burger.classList.toggle('translate-y-[250px]');
+}
+
+document.onclick = function (e) {
+  if (e.target.id !== "nav" && e.target.id !== "bb" && e.target.id !== "word" && e.target.id !== "logo" && e.target.id !== "fhic" && e.target.id !== "burger") {
+    burger.classList.remove("translate-y-[250px]");
+  }
+};
