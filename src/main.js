fe20673136ecfbdc830b749385216534861874ab
qwq-789 <elfrain520@gmail.com>
2022-03-20 23:12:01 +0800

    v5 各頁面選單完成

--- src/main.js
+++ src/main.js
@@ -2,14 +2,13 @@ import './style.css'
 
 var bb = document.getElementById('bb');
 var burger = document.getElementById('burger');
-var header = document.getElementsByTagName('header')
 
 bb.onclick = function(){
   burger.classList.toggle('translate-y-[250px]');
 }
 
 document.onclick = function (e) {
-  if (e.target.id !== "nav" && e.target.id !== "bb" && e.target.id !== "word" && e.target.id !== "logo" && e.target.id !== "fhic" && e.target.id !== "burger") {
+  if (e.target.id !== "nav" && e.target.id !== "bb" && e.target.id !== "word" && e.target.id !== "logo" && e.target.id !== "fhic" && e.target.id !== "burger" && e.target.id !== "li-a" && e.target.id !== "li-b" && e.target.id !== "li-c" && e.target.id !== "li-d" && e.target.id !== "li-e") {
     burger.classList.remove("translate-y-[250px]");
   }
 };
