efd97ba3c34162408465dac919720f921a4ac849
qwq-789 <elfrain520@gmail.com>
2022-07-21 01:32:03 +0800

    大小調整完畢 多頁面待研究

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-const { resolve } = require('path')
-const { defineConfig } = require('vite')
+const { resolve } = require("path");
+const { defineConfig } = require("vite");
 
 module.exports = defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        Marketing: resolve(__dirname, 'Marketing/index.html'),
-        M1: resolve(__dirname, 'Marketing/Gallery/About.html'),
-        M2: resolve(__dirname, 'Marketing/Gallery/Dream.html'),
-        M3: resolve(__dirname, 'Marketing/Gallery/MeetStudent.html'),
-        Technical: resolve(__dirname, 'Technical/index.html'),
-        T1: resolve(__dirname, 'Technical/Gallery/FhicClub.html'),
-        T2: resolve(__dirname, 'Technical/Gallery/MeetStudent.html')
-      }
-    }
-  }
-})
+        main: resolve(__dirname, "index.html"),
+        Marketing: resolve(__dirname, "Marketing/index.html"),
+        M1: resolve(__dirname, "Marketing/Gallery/About.html"),
+        M2: resolve(__dirname, "Marketing/Gallery/Dream.html"),
+        M3: resolve(__dirname, "Marketing/Gallery/MeetStudent.html"),
+        Technical: resolve(__dirname, "Technical/index.html"),
+        T1: resolve(__dirname, "Technical/Gallery/FhicClub.html"),
+        T2: resolve(__dirname, "Technical/Gallery/MeetStudent.html"),
+      },
+    },
+  },
+});
